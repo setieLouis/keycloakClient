f5c9be74d500f1547468b64653f0fd27c4f0de38
setieLouis <adjams@live.it>
2022-03-21 17:42:28 +0100

    add customer

--- src/main/java/com/example/simple_app/ControllerSome.java
+++ src/main/java/com/example/simple_app/ControllerSome.java
@@ -1,11 +1,14 @@
 package com.example.simple_app;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.keycloak.KeycloakPrincipal;
+import org.keycloak.representations.IDToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.security.Principal;
-import java.util.Collections;
 
 import static com.example.simple_app.Customer.*;
 
@@ -19,13 +22,27 @@ public class ControllerSome {
     
     
      @GetMapping("/customers")
-    public String customers(Principal principal, Model model){
-        addCustomer();
-        model.addAttribute("customers", customer());
-        model.addAttribute("username", "ciao");
+    public String customers(Principal principal, Model model) throws JsonProcessingException {
+        IDToken idtoken =  ((KeycloakPrincipal) principal).getKeycloakSecurityContext().getIdToken();
+        new ObjectMapper().writer().withDefaultPrettyPrinter().writeValueAsString(idtoken);
+        model.addAttribute("token",body(idtoken));
+        model.addAttribute( "username", "ciao");
         return "customers";        
      }
 
-    private void addCustomer() {
+    private String body(IDToken idtoken) {
+
+        StringBuffer bf = new StringBuffer("{\n");
+        bf.append(singleOne("name", idtoken.getGivenName()));
+        bf.append(singleOne("username", idtoken.getPreferredUsername()));
+        bf.append(singleOne("luckyNUmber", idtoken.getOtherClaims().get("MyLuckyNumber").toString()));
+        bf.append("}");
+        return bf.toString();
+    }
+
+
+
+    private String singleOne(String name , String value){
+         return "\"" + name + "\":" + " \""+ value + "\",\n";
     }
 }
